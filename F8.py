64206cb4bf738d748b9f7f160b2500887566ab6c
David <david.plnmr@eduge.ch>
2020-01-08 15:54:55 +0100

    Initial Commit F8

--- F8.py
+++ F8.py
@@ -2,30 +2,55 @@
 from tkinter import *
 from PIL import *
 
-mainview = Tk(className='CrowScape')
-#Force the window size at screen size
-mainview.geometry("{0}x{1}+0+0".format(mainview.winfo_screenwidth(), mainview.winfo_screenheight()))
+class UI:
+    
+    
+    def __init__(self):
+        self.mainview = Tk(className='CrowScape')
+        self.mainview.geometry("{0}x{1}+0+0".format(self.mainview.winfo_screenwidth(), self.mainview.winfo_screenheight()))    
+        
+        self.titleLabel = Label(self.mainview, text="Enigme 1", font=("Arial", 64))
+        self.enigmeLabel = Label(self.mainview, text="Indice pour enigme", font=("Arial", 48))
+        self.enigmeNumberLabel = Label(self.mainview, text="Numéro Enigme", font=("Arial", 32))
 
-title = StringVar()
-enigmeText = StringVar()
-enigmeNumberText = StringVar()
+    def SetTitle(self, textLabel):
+        self.titleLabel.config(text=textLabel)
+    
+    def SetEnigmeNumberText(self, textLabel):
+        self.enigmeNumberLabel.config(text=textLabel)
 
-titleLabel = Label(mainview, textvariable=title, font=("Arial", 64))
-titleLabel.pack()
+    def SetEnigmeText(self, textLabel):
+        self.enigmeLabel.config(text=textLabel)
 
-enigmeLabel = Label(mainview, textvariable=enigmeText, font=("Arial", 48))
-enigmeLabel.pack()
+    def PlaceAllLabels(self):
+        self.titleLabel.place(relx=.5, rely=.10, anchor="center")
+        self.enigmeLabel.place(relx=.5, rely=.20, anchor="center")
+        self.enigmeNumberLabel.place(relx=0.0, rely=1.0, anchor='sw')
+    
+    def Display(self):
+        self.PlaceAllLabels()
+        self.mainview.mainloop()
+    
 
-enigmeNumber = Label(mainview, textvariable=enigmeNumberText, font=("Arial", 32))
-enigmeNumber.place(relx=0.0, rely=1.0, anchor='sw')
 
-photo = PhotoImage(file="giphy.gif")
+"""userInterface = UI()
+
+userInterface.SetTitle("eowrzqw")
+userInterface.SetEnigmeText("")
+userInterface.SetEnigmeNumberText("Enigme XX/XX")
+
+userInterface.Display()"""
+
+
+
+
+
+"""photo = PhotoImage(file="giphy.gif")
 w = Label(mainview, image=photo)
-w.photo = photo
 w.pack()
 
 title.set("Test")
 enigmeText.set("Enigme Test")
 enigmeNumberText.set("Enigme XX/XX")
 
-mainview.mainloop()
+mainview.mainloop()"""
